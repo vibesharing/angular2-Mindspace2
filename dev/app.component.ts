5551209086f63d7db8bd9245ac069b20f5b6e854
vibesharing <buret.hadrien@orange.fr>
2016-06-03 21:03:50 +0200

    data bindings done

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -5,8 +5,19 @@ import {Component} from 'angular2/core';
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
+        <input type ="text" [ngClass]="{red: true}" [value]= "name" (keyup)="onKeyup(inputElement.value)" #inputElement>
+        <p>{{values}}</p>
+        {{Ontest()}}
     `,
 })
 export class AppComponent {
+    name = 'Maw';
+    values = '';
+    Ontest(){
+        return 1 === 1;
+    }
+onKeyup(value: string){
+    this.values += value + '|';
 
+}
 }

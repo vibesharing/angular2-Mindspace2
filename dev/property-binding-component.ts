c42f3976ccf84f9caea277d70dd2df71252afc51
vibesharing <buret.hadrien@orange.fr>
2016-06-03 21:40:55 +0200

    add new component

---
+++ dev/property-binding-component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-property-binding',
+  template:`
+  <h2>this is the child component</h2>
+  `
+})
+export class PropertyBindingComponent{
+
+}

1e317ede01c6549b301547e8588c01de84d6d5ba
vibesharing <buret.hadrien@orange.fr>
2016-06-12 15:31:00 +0200

    example of custom Pipe to reverse the order of letter

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,18 @@
 import {Component} from 'angular2/core';
+import {ReversePipe} from './reverse.pipe';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
+        <section>
+            <h1>Custom Pipe</h1>
+            <input type="text" (keyup)='0' #inputCustomPipe>
+            <h2>Output: {{inputCustomPipe.value | myReverse}}</h2>
+        </section>
     `,
+    pipes: [ReversePipe]
 })
 export class AppComponent {
 
---
+++ dev/reverse.pipe.ts
@@ -0,0 +1,18 @@
+import {Pipe} from 'angular2/core';
+import {PipeTransform} from 'angular2/core';
+
+@Pipe({
+  name: 'myReverse'
+})
+
+export class ReversePipe implements PipeTransform{
+  transform(value: string, args: any[]){
+    const length = value.length;
+    let resultString = '';
+    for (let i = 0; i < length; i++ ){
+      resultString = value[i] + resultString;
+    }
+    return resultString;
+
+  }
+}

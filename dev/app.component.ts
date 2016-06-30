0bcc8dbb7e09e6d9ec14ae10a66acdec2133bee3
vibesharing <buret.hadrien@orange.fr>
2016-06-30 14:30:34 +0200

    get done and post fail to send the right data

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,48 @@
 import {Component} from 'angular2/core';
+import {HttpService} from "./http.service";
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
+<div>
+    <div class="input">
+        <label for="title">Title</label>
+        <input type="text" id="name" #name>
+    </div>
+    <div class="input">
+        <label for="body">number</label>
+        <input type="number" id="number" #number>
+    </div>
+    <div class="input">
+        <label for="user-id">user ID</label>
+        <input type="text" id="user-id" #userId>
+    </div>
+    <button (click)='onPost(name.value, number.value)'> Post Data</button>
+    <button (click)='onGetPosts()'> Get all post</button>
+    <p>
+        Response: {{response | json}}
+    </p>
+</div>
+`,
+providers: [HttpService]
 })
-export class AppComponent {
 
+export class AppComponent {
+response: string;
+constructor(private _httpService: HttpService){}
+onGetPosts(){
+    this._httpService.getPosts()
+    .subscribe(
+        response =>this.response = response,
+        error => console.log(error)
+    )
+
+}
+onPost(name: string, numberr: string){
+    this._httpService.createPost({name: name, number: numberr})
+    .subscribe(
+        response => this.response = response,
+        error => console.log(error)
+    )
+}
 }
---
+++ dev/http.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
+import {Headers} from "angular2/http";
+@Injectable()
+export class HttpService{
+  constructor(private _http: Http){}
+    getPosts(): Observable<any> {
+      return this._http.get('http://localhost:8000/phones')
+      .map(res => res.json());
+    }
+    createPost(post: {name: string, number: string}): Observable<any>{
+      const body = JSON.stringify(post);
+      let headers = new Headers();
+      headers.append('content-Type','application/x-www-urlencoded');
+      return this._http.post('http://localhost:8000/phones', body, {
+        headers: headers
+      }).map(res => res.json());
+    }
+
+}

0e036b82104de533501791fbe9ca75ed90cea26e
vibesharing <buret.hadrien@orange.fr>
2016-06-30 11:56:53 +0200

    Last chance before goodbye

--- dev/component2.component.ts
+++ dev/component2.component.ts
@@ -1,14 +1,20 @@
 import {Component} from 'angular2/core';
-import {Router} from 'angular2/router'
+import {Router} from 'angular2/router';
+import {CanDeactivate} from 'angular2/router';
+import {ComponentInstruction} from 'angular2/router';
+
 @Component({
-  template: `
+    template: `
   <h2>This is Composent 2</h2>
   <button  (click)='onNavigate()'> Component 1</button>`
 })
 
-export class Component2Component{
-  constructor(private _router: Router){}
-    onNavigate(){
-      this._router.navigate(['Component1', {source:'Component 2'}]);
+export class Component2Component implements CanDeactivate {
+    constructor(private _router: Router) { }
+    onNavigate() {
+        this._router.navigate(['Component1', { source: 'Component 2' }]);
     }
-  }
+    routerCanDeactivate(nextInstruction: ComponentInstruction, prevInstruction: ComponentInstruction): any {
+        return alert("h");
+    }
+}
